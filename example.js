333e4665a5d91ef241af2b5b0d96515cfb3dd359
Tim Leerhoff <tleerhof@web.de>
2016-11-12 10:49:29 +0100

    Fixed codestyle.

--- example.js
+++ example.js
@@ -2,10 +2,11 @@ var PORT = 8000;
 var HOST = '127.0.0.1';
 var express = require('express');
 var http = require('http');
+var path = require('path');
 var app = express();
 
-var serverPath = __dirname + '/public';
-var leafletFile = __dirname + '/leaflet-tracksymbol.js';
+var serverPath = path.join(__dirname, 'public');
+var leafletFile = path.join(__dirname, 'leaflet-tracksymbol.js');
 
 app.use(express.static(serverPath));
 app.get('/leaflet-tracksymbol.js', function(req, res) {
